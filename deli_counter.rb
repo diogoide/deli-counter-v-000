cc062a62555c0b77e915ee2449e874520497fec5
Diogo Ide <diogoide@gmail.com>
2018-04-02 23:59:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 def take_a_number(katz_deli, name)
   if katz_deli.size == 0
     puts "Welcome, #{name}. You are number 1 in line."
-  else 
+  else
     number = katz_deli.size + 1
     puts "Welcome, #{name}. You are number #{number} in line."
   end 
